34dc953c5d3efcdcbac8f0f3b6be67a2989d1614
ali <abadakhshan@gmail.com>
2020-03-06 16:34:24 +0330

    add @DynamicForm("CustomEntityDetailComponent")

--- src/app/modules/admin/custom-entity/custom-entity-detail.component.ts
+++ src/app/modules/admin/custom-entity/custom-entity-detail.component.ts
@@ -1,16 +1,16 @@
 import { Component, Injector } from "@angular/core";
 import { NarikDetailForm } from "@narik/app-core";
 import { CustomEntity } from "./custom-entity";
-import { NarikBaseTemplate } from "@narik/core";
+import { NarikBaseTemplate, DynamicForm } from "@narik/core";
 import { validate, ValidationError } from "class-validator";
 import { formatString } from "@narik/common";
 
+@DynamicForm("CustomEntityDetailComponent")
 @NarikBaseTemplate("NarikDetailUi")
 @Component({
   templateUrl: "custom-entity-detail.component.html"
 })
 export class CustomEntityDetailComponent extends NarikDetailForm<CustomEntity> {
-  static readonly COMPONENT_NAME = "CustomEntityDetailComponent";
   constructor(injector: Injector) {
     super(injector);
   }
