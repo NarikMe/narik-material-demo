08c67f63970a4ff44efda7ce1a608be5173f7d9b
ali <abadakhshan@gmail.com>
2020-11-23 17:17:06 +0330

    add host token

--- src/app/modules/share/general-list/general-list.component.ts
+++ src/app/modules/share/general-list/general-list.component.ts
@@ -1,10 +1,17 @@
-import { Component, Injector } from "@angular/core";
-import { DynamicForm } from "@narik/core";
-import { NarikUiListForm } from "@narik/ui-lib";
+import { Component, forwardRef, Injector } from '@angular/core';
+import { DynamicForm } from '@narik/core';
+import { HOST_TOKEN } from '@narik/infrastructure';
+import { NarikUiListForm } from '@narik/ui-lib';
 
-@DynamicForm("GeneralListComponent")
+@DynamicForm('GeneralListComponent')
 @Component({
-  templateUrl: "general-list.component.html",
+  templateUrl: 'general-list.component.html',
+  providers: [
+    {
+      provide: HOST_TOKEN,
+      useExisting: forwardRef(() => GeneralListComponent),
+    },
+  ],
 })
 export class GeneralListComponent extends NarikUiListForm<any> {
   constructor(injector: Injector) {
