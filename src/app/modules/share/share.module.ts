4de055af73394f739be00c44c943d176ecf733ee
ali <abadakhshan@gmail.com>
2020-12-21 19:31:09 +0330

    double to single

--- src/app/modules/share/share.module.ts
+++ src/app/modules/share/share.module.ts
@@ -1,30 +1,30 @@
-import { COMPONENTS } from "./index";
-import { NgModule, Injectable } from "@angular/core";
-import { NarikUiMaterialModule } from "@narik/ui-material";
-import { CommonModule, DatePipe } from "@angular/common";
-import { ReactiveFormsModule } from "@angular/forms";
-import { MatCardModule } from "@angular/material/card";
+import { COMPONENTS } from './index';
+import { NgModule, Injectable } from '@angular/core';
+import { NarikUiMaterialModule } from '@narik/ui-material';
+import { CommonModule, DatePipe } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import {
   MAT_DATE_FORMATS,
   NativeDateAdapter,
   DateAdapter,
-} from "@angular/material/core";
+} from '@angular/material/core';
 
 export const PICK_FORMATS = {
-  parse: { dateInput: { month: "short", year: "numeric", day: "numeric" } },
+  parse: { dateInput: { month: 'short', year: 'numeric', day: 'numeric' } },
   display: {
-    dateInput: "input",
-    monthYearLabel: { year: "numeric", month: "short" },
-    dateA11yLabel: { year: "numeric", month: "long", day: "numeric" },
-    monthYearA11yLabel: { year: "numeric", month: "long" },
+    dateInput: 'input',
+    monthYearLabel: { year: 'numeric', month: 'short' },
+    dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
+    monthYearA11yLabel: { year: 'numeric', month: 'long' },
   },
 };
 
 @Injectable()
 export class PickDateAdapter extends NativeDateAdapter {
   format(date: Date, displayFormat: any): string {
-    if (displayFormat === "input") {
-      return new DatePipe("en-US").transform(date, "yyyy/MM/dd");
+    if (displayFormat === 'input') {
+      return new DatePipe('en-US').transform(date, 'yyyy/MM/dd');
     } else {
       return super.format(date, displayFormat);
     }
